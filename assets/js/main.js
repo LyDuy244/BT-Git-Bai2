ea39d2b19b706b21266125ba08dc9c6e3ee3e34f
Lý Nguyễn Ngọc Duy <118716140+LyDuy244@users.noreply.github.com>
2023-10-17 10:34:53 +0700

    update: JS change color.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,27 @@
 const boxs = document.querySelectorAll(".box")
 
-boxs.forEach(item => item.addEventListener("click", function(e){
-    if(e.target.classList.contains("box1")){
-        e.target.classList.toggle("yellow")
-    }
-    else if(e.target.classList.contains("box2")){
-        e.target.classList.toggle("pink")
-    }
-    else if(e.target.classList.contains("box3")){
-        e.target.classList.toggle("change-color")
-    }
+let arrIndex = [0, 1, 2]
+let arrColors = ["#ff7e00", "#ffcc01", "#e94b86"]
+
+boxs.forEach(item => item.addEventListener("click", function (e) {
+    handleChangeBackground(arrIndex, boxs)
 }))
+
+function handleChangeBackground(arrIndex, boxs) {
+    let i = 0;
+    while (i < arrIndex.length) {
+        if (i === arrIndex.length - 1) {
+            break;
+        }
+        [arrIndex[i], arrIndex[i + 1]] = [arrIndex[i + 1], arrIndex[i]]
+        console.log(arrIndex)
+        i++;
+    }
+
+
+    for (let i = 0; i < boxs.length; i++) {
+        boxs[i].style.backgroundColor = arrColors[arrIndex[i]];
+    }
+}
+
+
